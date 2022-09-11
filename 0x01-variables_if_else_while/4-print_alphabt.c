ca88cb4f6242b1e76acd9c9a303fd23471044e69
root <keringcaleb3@gmail.com>
2022-09-11 16:52:08 -0700

    changes on 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 if (ch != 'e' && ch != 'q')
 {
 	putchar(ch);
 }
 }
-putchar("\n");
-return(0);
+putchar('\n');
+return (0);
 }
