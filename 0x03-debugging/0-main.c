097247d0150ae9fe5e78a2fd2691ae13e59e9517
root <keringcaleb3@gmail.com>
2022-09-15 05:34:17 -0700

    changes on main.c and .h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -20,38 +20,7 @@ int main(void)
 
 
 
-        i = 98;
-
-        positive_or_negative(i);
-
-
-
-        return (0);
-
-}
-#include "main.h"
-
-
-
-/**
-
-* main - tests function that prints if integer is positive or negative
-
-* Return: 0
-
-*/
-
-
-
-int main(void)
-
-{
-
-        int i;
-
-
-
-        i = 98;
+        i = 0;
 
         positive_or_negative(i);
 
