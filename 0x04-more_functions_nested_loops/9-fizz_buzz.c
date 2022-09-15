c134457a1ce8876fc56e6c17370d25b028834546
root <keringcaleb3@gmail.com>
2022-09-15 09:04:20 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,7 +3,7 @@
 /**
  * main - Program to print fizz buzz
  *
- * Return: 0
+ * Return: 0 for success
  */
 
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0)
+		if ((num % 3 == 0) && (num % 5 == 0))
 			printf("%s", "FizzBuzz");
 		else if (num % 5 == 0)
 			printf("%s", "Buzz");
