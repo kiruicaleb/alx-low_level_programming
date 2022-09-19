838e24d98efbaab50a42109eba686d5baf8ace2e
root <keringcaleb3@gmail.com>
2022-09-19 15:34:03 -0700

    printing a string and knowing its length

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half -  prints half a line of strings
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int i, len = _strlen(str);
+
+	for (i = ((len - 1) / 2) + 1; i < len; i++;
+		putchar(*(str + i));
+	putchar(10);
+}
+
+/**
+ * _strlen - prints length of a string
+ * @s: string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
