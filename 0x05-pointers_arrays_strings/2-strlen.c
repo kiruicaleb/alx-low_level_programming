72ce4606ce1178f7aa9e7999ae9c8e4d32029b03
root <keringcaleb3@gmail.com>
2022-09-19 14:19:51 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Pfogram to return the length of an string
+ * @s: string
+ * Return: returns length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
