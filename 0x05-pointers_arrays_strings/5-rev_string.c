4ed7949768e221774da04621ce838067cba0a686
root <keringcaleb3@gmail.com>
2022-09-19 16:07:31 -0700

    reverses string and prints its length

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,38 @@
-#include 
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i, tmp, len = _strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
+}
+
+/**
+ * _strlen - prints length of string
+ * @s: string
+ *
+ * Return: length of given string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
