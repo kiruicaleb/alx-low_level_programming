5197e41343513e783b4062f6b789320a75edfe6d
root <keringcaleb3@gmail.com>
2022-09-19 13:30:24 -0700

    pointer to copy paste string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,16 +4,20 @@
  * _strcpy - Program to add a string at the end of another string
  * @dest: Pointer to string to be copied to
  * @src: Pointer to string to be copied
+ *
  * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int idx;
+	int inc = 0;
 
-	for (idx = 0; src[idx] != '\0'; idx++)
-		dest[idx] = src[idx];
+	while (*(src + inc) != '\0')
+	{
+		*(dest + inc) = *(src + inc);
+		inc++;
+	}
+	*(dest + inc) = '\0';
 
-	dest[idx] = '\0';
 	return (dest);
 }
