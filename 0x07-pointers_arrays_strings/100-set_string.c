e31196a0e24605ffd83bd0e469342a987cd5a8cd
root <keringcaleb3@gmail.com>
2022-09-27 14:35:46 -0700

    function that sets the alue of a pointer to a character

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Function setting value of pointer to a char
+ * @s: Pointe to pointer of string
+ * @to: Pointer to string
+ * Return:void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+void set_string(char **s, char *to);
+
+#endif
