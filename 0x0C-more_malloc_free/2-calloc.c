40cb5acdb7ee5bcd1b80d3152f971adb230aadf5
Karimkering <keringcaleb3@gmail.com>
2022-10-06 16:54:58 -0700

    function that allocates memory for array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,6 +3,7 @@
 
 /**
  * _calloc - Function that allocates memory for array using malloc
+ *
  * @nmemb: Int for number of elements in the array
  * @size: Int for size of each element in array
  * Return: Pointer value to the allocated memory
