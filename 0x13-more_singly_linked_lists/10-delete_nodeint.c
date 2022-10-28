07443bffd10ef62264a557c5bc9a3e5685a1b243
Karimkering <keringcaleb3@gmail.com>
2022-10-28 10:40:21 -0700

    function that reverses list of integers

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h" 
+
+/**                                                                                              
+ * delete_nodeintat_index - Function that deletes the node at
+ * index index of a listint_t linked list
+ *
+ * @head: Pointer to pointer to first list element
+ * @index: Index to list
+ * Return: 1 for success, -1 for failed
+ */
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+function that finds loops in linked lists
