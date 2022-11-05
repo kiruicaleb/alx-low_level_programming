98ee8433c23c8d8085ceab7e53892c7cc999220e
Karimkering <keringcaleb3@gmail.com>
2022-11-05 06:47:33 -0700

    function that reads a file and prints it to POIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * read_textfile - Function that reads a text file and
@@ -11,27 +12,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *ltrs = malloc(sizeof(char) * letters);
-	int f_open, f_read, f_write;
+	int file, n_read, wrote;
+	char *buffer;
 
-	if (filename == NULL)
-		return (0);
-	f_open = open(filename, O_RDONLY);
-	if ((f_open == -1) || (ltrs == NULL))
-		return (0);
-	f_read = read(f_open, ltrs, letters);
-	if (f_read == -1)
+	buffer = malloc(sizeof(*buffer) * (letters + 1));
+	if (filename == NULL || buffer == NULL)
 	{
-		free(ltrs);
+		free(buffer);
 		return (0);
 	}
-	f_write = write(STDOUT_FILENO, ltrs, f_read);
-	if (f_write == -1)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 	{
-		free(ltrs);
 		return (0);
 	}
-	free(ltrs);
-	close(f_open);
-	return (f_write);
+	n_read = read(file, buffer, letters);
+	if (n_read == -1)
+		return (0);
+	buffer[n_read] = '\0';
+	wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (wrote != n_read)
+		return (0);
+	free(buffer);
+	close(file);
+	return (n_read);
 }
