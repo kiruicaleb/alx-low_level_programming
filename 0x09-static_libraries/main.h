11a489b21e6de2deb252884c85d31f7e35afea19
kennynyamai <kennynamai73@gmail.com>
2023-06-03 23:01:02 +0300

    corrected

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef __MAIN__
-#define __MAIN__
+#ifndef MAIN_H
+#define MAIN_H
+
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,7 +22,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int nrandom();
 
-
-#endif /* __MAIN__ */
+#endif /* MAIN_H */
